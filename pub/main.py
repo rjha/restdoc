fa3e389570b07d0cb06d0a10049d59aaffde6502
Mitesh Banka <banka.mitesh@gmail.com>
2018-04-13 15:20:44 +0530

    argparse arguments modified

--- pub/main.py
+++ pub/main.py
@@ -6,20 +6,25 @@ from jinja2 import Template
 
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("Yaml_File", help="Path of the Yaml File")
-parser.add_argument("out_dir", help="Output Directory")
-parser.add_argument("out_type", choices=["PDF", "HTML", "BOTH"], help="Output Type - PDF, HTML or BOTH")
+parser = argparse.ArgumentParser(description="Restdoc API Documentation Python Script")
+parser.add_argument("-s", "--source", dest="source", default="index.yaml", help="Source Yaml File")
+parser.add_argument("-d", "--dest", dest="dest", default="Documents/", help="Destination Directory")
+parser.add_argument("-f", "--format", default="BOTH", choices=["PDF", "HTML", "BOTH"], help="Format of Output File")
+parser.add_argument("-t", "--test", action="store_true", default=False, help="Test the python script with sample yaml file")
 args = parser.parse_args()
 
-stream = open(args.Yaml_File, "r")
+if args.test:
+    stream = open("yaml/sample1.yaml", "r")
+else :
+    stream = open(args.source, "r")
+
 items = yaml.load(stream)
 
 with open('templates/document.tmpl') as t:
     tmpl = Template(t.read())
 
-if args.out_type == "PDF" or args.out_type == "BOTH" :
-    filename=args.out_dir+items["name"]+".pdf"
+if args.format == "PDF" or args.format == "BOTH" :
+    filename=args.dest+items["name"]+".pdf"
     options = {
         'page-size': 'A4',
         'dpi': 300,
@@ -31,8 +36,8 @@ if args.out_type == "PDF" or args.out_type == "BOTH" :
     }
     pdfkit.from_string(tmpl.render(items=items), filename, options=options)
 
-if args.out_type == "HTML" or args.out_type == "BOTH":
-    filename=args.out_dir+items["name"]+".html"
+if args.format == "HTML" or args.format == "BOTH":
+    filename=args.dest+items["name"]+".html"
 
     f = open(filename,"w")
     f.truncate()
