b4d3bcfb7a0d75e209d306740d3ce5be87ec95f9
Mitesh Banka <banka.mitesh@gmail.com>
2018-04-13 18:33:35 +0530

    argparse modification and changed the structure

--- README.md
+++ README.md
@@ -19,11 +19,21 @@ read API definition from YAML files and generates HTML and PDF.
 How?
 =======
 
-Clone the repo and dump the restdoc folder anywhere you like. Please make sure you have python (www.python.org) and pdfkit (https://pypi.python.org/pypi/pdfkit), wkhtmltopdf(https://pypi.python.org/pypi/wkhtmltopdf/0.1), yaml (https://pyyaml.org/wiki/PyYAMLDocumentation), jinja2 (http://jinja.pocoo.org) and argparse (https://pypi.python.org/pypi/argparse) modules installed in your system. We are assuming that you know how to run Python scripts. Simply run the python script "main.py" in /pub folder with the arguments "Yaml_File" - Source Yaml file, "out_dir" - Destination output directory and "out_type" - Type of the generated output file "PDF" for a PDF Document, "HTML" for a HTML Document and "BOTH" for both types.
+Clone the repo and dump the restdoc folder anywhere you like. 
 
-Example - python main.py yaml/sample1.yaml ~/Desktop/ PDF
+Please make sure you have pdfkit (https://pypi.python.org/pypi/pdfkit), wkhtmltopdf(https://pypi.python.org/pypi/wkhtmltopdf/0.1), yaml (https://pyyaml.org/wiki/PyYAMLDocumentation) and jinja2 (http://jinja.pocoo.org) python modules installed in your system. 
 
-Sample API definition resides in the YAML files inside yaml folder. You can see the samples to get started. 
+We are assuming that you know how to run a Python script. Place all your YAML files in src directory. Simply run the python script main.py in pub folder. HTML and PDF file for all the YAML files will generated in the dest folder. To change the source or destination directory use the options given below.
+
+Options -
+
+-h or --help : Help and list of options.
+-t or --test : Test with our sample YAML file. Parsed sample HTML and PDF file will be created in dest folder
+-s or --source : Source directory of YAML files to be parsed. Default - src.
+-d or --dest : Destination directory for parsed HTML and PDF files. Default - dest.
+-f or --format : Format of the parsed file (PDF, HTML or BOTH). Default - BOTH.
+
+Sample API definition resides in the YAML files inside sample folder. You can see the samples to get started. 
 We use Twitter Bootstrap library and the Look and Feel of API pages is modeled on twitter REST API.
 
 Contact
@@ -33,5 +43,4 @@ Mitesh Banka
 +91 8482096370
 mbanka@yuktix.com
 www.yuktix.com
-
 
--- pub/main.py
+++ pub/main.py
@@ -1,47 +1,70 @@
+import time
+time_start = time.clock()
+
+import argparse
+
+import glob
+
 import yaml
+
 import pdfkit
 
 import jinja2
 from jinja2 import Template
 
-import argparse
 
-parser = argparse.ArgumentParser(description="Restdoc API Documentation Python Script")
-parser.add_argument("-s", "--source", dest="source", default="index.yaml", help="Source Yaml File")
-parser.add_argument("-d", "--dest", dest="dest", default="Documents/", help="Destination Directory")
-parser.add_argument("-f", "--format", default="BOTH", choices=["PDF", "HTML", "BOTH"], help="Format of Output File")
-parser.add_argument("-t", "--test", action="store_true", default=False, help="Test the python script with sample yaml file")
-args = parser.parse_args()
 
-if args.test:
-    stream = open("yaml/sample1.yaml", "r")
-else :
-    stream = open(args.source, "r")
+def yaml_parse(dest, format, source) :
 
-items = yaml.load(stream)
+    stream = open(source, "r")
 
-with open('templates/document.tmpl') as t:
-    tmpl = Template(t.read())
+    items = yaml.load(stream)
 
-if args.format == "PDF" or args.format == "BOTH" :
-    filename=args.dest+items["name"]+".pdf"
-    options = {
-        'page-size': 'A4',
-        'dpi': 300,
-        'margin-top': '0.75in',
-        'margin-right': '0.75in',
-        'margin-bottom': '0.75in',
-        'margin-left': '0.75in',
-        'encoding': "UTF-8",
-    }
-    pdfkit.from_string(tmpl.render(items=items), filename, options=options)
+    with open('templates/document.tmpl') as t:
+        tmpl = Template(t.read())
 
-if args.format == "HTML" or args.format == "BOTH":
-    filename=args.dest+items["name"]+".html"
+    if format == "PDF" or format == "BOTH" :
+        filename=dest+"/"+items["name"]+".pdf"
+        options = {
+            'page-size': 'A4',
+            'dpi': 300,
+            'margin-top': '0.75in',
+            'margin-right': '0.75in',
+            'margin-bottom': '0.75in',
+            'margin-left': '0.75in',
+            'encoding': "UTF-8",
+        }
+        pdfkit.from_string(tmpl.render(items=items), filename, options=options)
 
-    f = open(filename,"w")
-    f.truncate()
+    if format == "HTML" or format == "BOTH":
+        filename=dest+"/"+items["name"]+".html"
 
-    f.write(tmpl.render(items=items))
-    f.close()
+        f = open(filename,"w")
+        f.truncate()
 
+        f.write(tmpl.render(items=items))
+        f.close()
+    return
+
+def main() :
+
+    parser = argparse.ArgumentParser(description="Restdoc API Documentation Python Script")
+    parser.add_argument("-s", "--source", dest="source", default="src", help="Source YAML Directory, default is src")
+    parser.add_argument("-d", "--dest", dest="dest", default="dest", help="Destination Directory, default if dest")
+    parser.add_argument("-f", "--format", default="BOTH", choices=["PDF", "HTML", "BOTH"], help="Format of Output File, default is BOTH")
+    parser.add_argument("-t", "--test", action="store_true", default=False, help="Test with our sample YAML files")
+    args = parser.parse_args()
+
+    if args.test:
+        sources = glob.glob("sample/*.yaml")
+    else:
+        sources = glob.glob(args.source+"/*.yaml")
+
+    for source in sources :
+        yaml_parse(args.dest, args.format, source)
+
+    time_elapsed = (time.clock() - time_start)
+    print "Total time taken - ", time_elapsed
+
+if __name__ == "__main__" :
+    main()
